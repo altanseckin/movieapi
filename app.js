88c05f3e778a8827abbbaa14d8d5409ab33d04c8
Altan <tuba.saday@hotmail.com>
2019-10-16 12:32:43 +0300

    değişiklikler

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const index = require('./routes/index');
 const movie = require('./routes/movie');
